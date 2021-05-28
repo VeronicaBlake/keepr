4d7f26d3bd107a0b1c221fb7728198979bd1f89c
Veronica Blake/Noyce <vrex149@gmail.com>
2021-05-28 15:52:42 -0600

    workspace debachle

--- server/Repositories/ProfilesRepository.cs
+++ server/Repositories/ProfilesRepository.cs
@@ -1,7 +1,33 @@
+using System;
+using System.Data;
+using Dapper;
+using server.Models;
+
 namespace server.Repositories
 {
     public class ProfilesRepository
     {
-        
+        private readonly IDbConnection _db;
+
+        public ProfilesRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+        internal Profile GetById(string id)
+        {
+            string sql = "SELECT * FROM Profiles WHERE id = @id";
+            return _db.QueryFirstOrDefault<Profile>(sql, new { id });
+        }
+
+        internal Profile Create(Profile newProfile)
+        {
+            string sql = @"
+            INSERT INTO Profiles
+              (name, picture, email, id)
+            VALUES
+              (@Name, @Picture, @Email, @Id)";
+            _db.Execute(sql, newProfile);
+            return newProfile;
+        }
     }
 }
--- server/Services/ProfilesService.cs
+++ server/Services/ProfilesService.cs
@@ -1,13 +1,24 @@
 using System;
 using server.Models;
+using server.Repositories;
 
 namespace server.Services
 {
     public class ProfilesService
     {
-        internal object GetOrCreateProfile(Profile userInfo)
+        private readonly ProfilesRepository _pr;
+        public ProfilesService(ProfilesRepository pr)
         {
-            throw new NotImplementedException();
+            _pr = pr;
+        }
+        internal Profile GetOrCreateProfile(Profile userInfo)
+        {
+            Profile profile = _pr.GetById(userInfo.Id);
+            if (profile == null)
+            {
+                return _pr.Create(userInfo);
+            }
+            return profile;
         }
 
     }
