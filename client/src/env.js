6c37ba66131cc5d5916ed5cb32987212a3384ac2
Veronica Blake/Noyce <vrex149@gmail.com>
2021-05-28 11:00:21 -0600

    init

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-k8azdd8c.us.auth0.com'
+export const audience = 'https://bugger.com'
+export const clientId = 'jgwvCI4WY9O6hnloTQMGOjatnfT2hCwe'
